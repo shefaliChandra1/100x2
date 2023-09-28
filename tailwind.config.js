cbcc153d6bb3471f71a8dd9135cef0014e3d6a51
Shefali P Chandra <shefalipchandra@Shefalis-MacBook-Air.local>
2023-09-28 21:02:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {}, 
+      fontFamily: {
+        "px-regular": "Inter",
+      }, 
+      colors: {
+          "neutral-50": "#F9F9F9",
+          "neutral-1000": "#000", 
+          "neutral-200": "#E4E4E4", 
+          "neutral-500": "#737373", 
+          "twitter-blue": "#1D9BF0",
+          "twtr-blue--hover": "#1871CA",
+          "twtr-blue--hover": "#1871CA", 
+      },
+      width: {
+          "334": "334px", 
+  
+      }, 
+      }, 
+        
+      },
+      plugins: [],
+      }
+      
+
+    
+
